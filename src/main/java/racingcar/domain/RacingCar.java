8ea38e16a99cb5c098d72dc218eadd0a6c5b395c
sangchu <didi1484@gmail.com>
2023-11-20 15:47:50 +0900

    feat: 사용자 입력 -> 자동차 리스트 전환 메소드 추가

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -1,4 +1,10 @@
 package racingcar.domain;
 
 public class RacingCar {
+    private final CarName carName;
+    private int distance = 0;
+
+    public RacingCar(CarName carName) {
+        this.carName = carName;
+    }
 }
