e0e61583a291fb8a6a30b6676cdf604fed6e4805
sangchu <didi1484@gmail.com>
2023-11-20 16:43:39 +0900

    feat: 자동차 전진 메소드 추가

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -1,11 +1,26 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RacingCar {
+    private static final int MIN_RANDOM_NUMBER = 0;
+    private static final int MAX_RANDOM_NUMBER = 9;
+    private static final int MOVABLE_NUMBER = 4;
     private final CarName carName;
     private int distance = 0;
 
     public RacingCar(CarName carName) {
         this.carName = carName;
+    }
+
+    public void moveForward(int randomNumber) {
+        if (randomNumber >= MOVABLE_NUMBER) {
+            distance++;
+        }
+    }
+
+    private int generateRandomNumber() {
+        return Randoms.pickNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
     }
 
     public String getCarName() {
