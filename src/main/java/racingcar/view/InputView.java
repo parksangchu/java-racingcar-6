a6d7c900869b3e869e41f227c0722da0f4582931
sangchu <didi1484@gmail.com>
2023-11-20 15:01:56 +0900

    feat: 자동차 이름 입력 받는 메소드 추가

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,13 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private static final String ASK_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String ASK_TURN = "시도할 회수는 몇회인가요?";
+
+    public static String readCarNames() {
+        System.out.println(ASK_CAR_NAMES);
+        return Console.readLine();
+    }
+}
