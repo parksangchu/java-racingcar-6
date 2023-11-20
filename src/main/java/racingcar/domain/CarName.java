e703bb4685c8facd35bc8003cd23be2d57d614d6
sangchu <didi1484@gmail.com>
2023-11-20 15:29:55 +0900

    feat: 차량 이름 길이, 특수문자 검증 메서드 추가

--- src/main/java/racingcar/domain/CarName.java
+++ src/main/java/racingcar/domain/CarName.java
@@ -1,4 +1,31 @@
 package racingcar.domain;
 
+import static racingcar.domain.ErrorMessage.INVALID_NAME_LENGTH;
+import static racingcar.domain.ErrorMessage.INVALID_NAME_TYPE;
+
+import java.util.regex.Pattern;
+
 public class CarName {
+    private static final int MIN_LENGTH = 1;
+    private static final int MAX_LENGTH = 5;
+    private static final String REGEX = "^[0-9]*[가-힣]*[a-zA-Z]*$";
+    private final String carName;
+
+    public CarName(String carName) {
+        validateLength(carName);
+        validateType(carName);
+        this.carName = carName;
+    }
+
+    private void validateLength(String carName) {
+        if (carName.length() < MIN_LENGTH || carName.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException(INVALID_NAME_LENGTH.getMessage());
+        }
+    }
+
+    private void validateType(String carName) {
+        if (!Pattern.matches(REGEX, carName)) {
+            throw new IllegalArgumentException(INVALID_NAME_TYPE.getMessage());
+        }
+    }
 }
--- src/main/java/racingcar/domain/ErrorMessage.java
+++ src/main/java/racingcar/domain/ErrorMessage.java
@@ -2,11 +2,16 @@ package racingcar.domain;
 
 public enum ErrorMessage {
     ERROR("[ERROR] "),
-    INVALID_NAME_LENGTH(ERROR + "차량 이름은 1-5글자 사이여야 합니다.");
+    INVALID_NAME_LENGTH(ERROR + "차량 이름은 1-5글자 사이여야 합니다."),
+    INVALID_NAME_TYPE(ERROR + "차량 이름은 특수문자를 포함할 수 없습니다.");
 
     private final String message;
 
     private ErrorMessage(String message) {
         this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
     }
 }
