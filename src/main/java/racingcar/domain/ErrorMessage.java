a2bbb793b2afccb29ede6146883d9f263abab3d1
sangchu <didi1484@gmail.com>
2023-11-20 16:06:49 +0900

    feat: 자동차 수량 검증 메서드 추가

--- src/main/java/racingcar/domain/ErrorMessage.java
+++ src/main/java/racingcar/domain/ErrorMessage.java
@@ -3,7 +3,8 @@ package racingcar.domain;
 public enum ErrorMessage {
     ERROR("[ERROR] "),
     INVALID_NAME_LENGTH(ERROR + "차량 이름은 1-5글자 사이여야 합니다."),
-    INVALID_NAME_TYPE(ERROR + "차량 이름은 특수문자를 포함할 수 없습니다.");
+    INVALID_NAME_TYPE(ERROR + "차량 이름은 특수문자를 포함할 수 없습니다."),
+    INVALID_RACING_CARS_SIZE(ERROR + "경주에는 최소 2대의 차량이 참여해야합니다.");
 
     private final String message;
 
--- src/main/java/racingcar/domain/RacingCars.java
+++ src/main/java/racingcar/domain/RacingCars.java
@@ -1,4 +1,21 @@
 package racingcar.domain;
 
+import static racingcar.domain.ErrorMessage.INVALID_RACING_CARS_SIZE;
+
+import java.util.List;
+
 public class RacingCars {
+    private static final int MIN_SIZE = 2;
+    private final List<RacingCar> racingCars;
+
+    public RacingCars(List<RacingCar> racingCars) {
+        validateSize(racingCars);
+        this.racingCars = racingCars;
+    }
+
+    private void validateSize(List<RacingCar> racingCars) {
+        if (racingCars.size() < MIN_SIZE) {
+            throw new IllegalArgumentException(INVALID_RACING_CARS_SIZE.getMessage());
+        }
+    }
 }
